ae1fc0956f082a0f032a3c4497429a4a04572ba4
Reggie <reginaldzacc@yahoo.com>
2019-12-18 09:58:58 -0600

    initial commit

---
+++ tacos.py
@@ -0,0 +1,29 @@
+'''
+Reginald Zaccardi-Richey
+Random Taco Cook Book Generator
+'''
+import requests
+import docx
+
+document = docx.newdocument()
+
+url = 'https://taco-1150.herokuapp.com/random/?full_taco=true'
+
+
+def main(url):
+    try:
+        data = requests.get(url).json()
+        seasoning = (data["seasoning"]["recipe"])
+        condiment = (data["condiment"]["recipe"])
+        mixin = (data["mixin"]["recipe"])
+        base_layer = (data["base_layer"]["recipe"])
+        document.add_paragraph(f'{seasoning}\n{condiment}\n{mixin}\n{base_layer}')
+    except ConnectionError:  # output if Connection Error
+        print('Connection Error')
+    except TimeoutError:  # output if Timeout error
+        print('Timeout Error')
+    except:  # broad error output
+        print('Error')
+
+
+main(url)
