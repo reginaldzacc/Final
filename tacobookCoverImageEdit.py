9087017a12dc2d06869514d35ea2f2764e416a36
Reggie <reginaldzacc@yahoo.com>
2019-12-20 12:43:45 -0600

    initial commit

--- tacobookCoverImageEdit.py
+++ tacobookCoverImageEdit.py
@@ -9,13 +9,13 @@ TacoBook Cover Image Editor
 '''
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open('tacoBOI.jpg')
+image = Image.open('tacoCover2.jfif')
 
-image.thumbnail((800,800))
+image.thumbnail((400,800))
 
 imageDraw = ImageDraw.Draw(image)
 
-font = ImageFont.truetype('DejaVuSans.ttf', 25)
-imageDraw.text([200, 25], 'RANDOM\nTACO RECIPE\nCOOKBOOK', fill='Fuchsia', font=font)
+font = ImageFont.truetype('DejaVuSans.ttf', 50)
+imageDraw.text([20, 25], 'RANDOM\nTACO\nCOOKBOOK', fill='DarkViolet', font=font)
 # save new image
 image.save('tacobookCover.jpeg')
--- tacos.py
+++ tacos.py
@@ -22,21 +22,38 @@ This block of code is where the main() function is.
 This is where most of the code will be run.
 this block of code will try to request JSON data 
 using our URL, Pull the data from that request, and 
-add a paragraph to word with the recipe. If there are 
+add a paragraphs and headers to word with the recipe. If there are 
 errors the program will tell the user instead of crashing.
 '''
 
 
 def main(url):  # creates main() function
     try:  # this line 'trys' to run the following code
-        data = requests.get(url).json()  # this line returns our API request as the variable 'data'
+        data = requests.get(url).json()  # this line returns our API request as the variable 'data' using our URL variable
+
+        seasoningName = (data["seasoning"]["name"])
+        condimentName = (data["condiment"]["name"])
+        mixinName = (data["mixin"]["name"])
+        base_layerName = (data["base_layer"]["name"])
+        shellName = (data["shell"]["name"])
         seasoning = (data["seasoning"]["recipe"])  # creates seasoning variable with information from our returned data
         condiment = (data["condiment"]["recipe"])  # creates condiment variable with information from our returned data
         mixin = (data["mixin"]["recipe"])  # creates mixin variable with information from our returned data
-        base_layer = (
-            data["base_layer"]["recipe"])  # creates base layer variable with information from our returned data
-        document.add_paragraph(
-            f'{seasoning}\n{condiment}\n{mixin}\n{base_layer}')  # this line prints our 4 variables onto our word document with a new line after each variable
+        base_layer = (data["base_layer"]["recipe"])  # creates base layer variable with information from our returned data
+        shell = (data["shell"]["recipe"])  # creates shell variable with information from our returned data
+
+        document.add_heading(f'{base_layerName} with {mixinName}, {seasoningName} and {condimentName} in {shellName}', 2)
+        document.add_heading(f'{base_layerName}', 4)
+        document.add_paragraph(f'{base_layer}')
+        document.add_heading(f'{mixinName}', 4)
+        document.add_paragraph(f'{mixin}')
+        document.add_heading(f'{seasoningName}', 4)
+        document.add_paragraph(f'{seasoning}')
+        document.add_heading(f'{condimentName}', 4)
+        document.add_paragraph(f'{condiment}')
+        document.add_heading(f'{shellName}', 4)
+        document.add_paragraph(f'{shell}')
+        document.add_page_break()
     except ConnectionError:  # output if Connection Error
         print('Connection Error')  # what user sees if Connection Error
     except TimeoutError:  # output if Timeout error
@@ -63,7 +80,13 @@ while True:
 This block is meant to generate the tacobook using main looped as many times as the user wanted. 
 '''
 print('Generating TacoBook')#lets user know the tacobook is being generated
-
+document.add_heading('Random Taco Cookbook', 0)
+document.add_picture('tacobookCover.jpeg')
+document.add_heading('Credits', 1)
+#image
+#api
+#author
+document.add_page_break()
 for number in range(recipes):#for loop to create recipes
     main(url)#runs our main function
 document.save('TacoBook.docx')#saves the document under file name "TacoBook.docx
