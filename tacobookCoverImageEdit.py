a861b3f67aef536ecd52cdbdac9fc83fbaa318a2
Reggie <reginaldzacc@yahoo.com>
2019-12-20 13:06:38 -0600

    initial commit

--- tacobookCoverImageEdit.py
+++ tacobookCoverImageEdit.py
@@ -5,17 +5,15 @@ TacoBook Cover Image Editor
 
 
 '''
-
+This short program takes a saved image,
+opens the image, resizes the image to a 
+smaller size,then writes RANDOM TACO 
+COOKBOOK on the image
 '''
 from PIL import Image, ImageDraw, ImageFont
-
-image = Image.open('tacoCover2.jfif')
-
-image.thumbnail((400,800))
-
-imageDraw = ImageDraw.Draw(image)
-
-font = ImageFont.truetype('DejaVuSans.ttf', 50)
-imageDraw.text([20, 25], 'RANDOM\nTACO\nCOOKBOOK', fill='DarkViolet', font=font)
-# save new image
-image.save('tacobookCover.jpeg')
+image = Image.open('tacoCover2.jfif')#opens saved cover image
+image.thumbnail((500,500))#resize image to 500x500
+imageDraw = ImageDraw.Draw(image)#allows drawing on image
+font = ImageFont.truetype('DejaVuSans.ttf', 50)#creates a font to use
+imageDraw.text([20, 25], 'RANDOM\nTACO\nCOOKBOOK', fill='DarkViolet', font=font)#writes title on image using font variable
+image.save('tacobookCover.jpeg')#save new image
--- tacos.py
+++ tacos.py
@@ -31,29 +31,29 @@ def main(url):  # creates main() function
     try:  # this line 'trys' to run the following code
         data = requests.get(url).json()  # this line returns our API request as the variable 'data' using our URL variable
 
-        seasoningName = (data["seasoning"]["name"])
-        condimentName = (data["condiment"]["name"])
-        mixinName = (data["mixin"]["name"])
-        base_layerName = (data["base_layer"]["name"])
-        shellName = (data["shell"]["name"])
+        seasoningName = (data["seasoning"]["name"])# creates seasoningName variable with information from our returned data
+        condimentName = (data["condiment"]["name"])# creates condimentName variable with information from our returned data
+        mixinName = (data["mixin"]["name"])# creates mixinName variable with information from our returned data
+        base_layerName = (data["base_layer"]["name"])# creates base layerName variable with information from our returned data
+        shellName = (data["shell"]["name"])# creates shellName variable with information from our returned data
         seasoning = (data["seasoning"]["recipe"])  # creates seasoning variable with information from our returned data
         condiment = (data["condiment"]["recipe"])  # creates condiment variable with information from our returned data
         mixin = (data["mixin"]["recipe"])  # creates mixin variable with information from our returned data
         base_layer = (data["base_layer"]["recipe"])  # creates base layer variable with information from our returned data
         shell = (data["shell"]["recipe"])  # creates shell variable with information from our returned data
 
-        document.add_heading(f'{base_layerName} with {mixinName}, {seasoningName} and {condimentName} in {shellName}', 2)
-        document.add_heading(f'{base_layerName}', 4)
-        document.add_paragraph(f'{base_layer}')
-        document.add_heading(f'{mixinName}', 4)
-        document.add_paragraph(f'{mixin}')
-        document.add_heading(f'{seasoningName}', 4)
-        document.add_paragraph(f'{seasoning}')
-        document.add_heading(f'{condimentName}', 4)
-        document.add_paragraph(f'{condiment}')
-        document.add_heading(f'{shellName}', 4)
-        document.add_paragraph(f'{shell}')
-        document.add_page_break()
+        document.add_heading(f'{base_layerName} with {mixinName}, {seasoningName} and {condimentName} in {shellName}', 0)#prints the heading for each recipe to doc
+        document.add_heading(f'{base_layerName}', 1)#prints base layer heading to doc
+        document.add_paragraph(f'{base_layer}')#prints base layer recipe to doc
+        document.add_heading(f'{mixinName}', 1)#prints mixin heading to doc
+        document.add_paragraph(f'{mixin}')#prints mixin recipe to doc
+        document.add_heading(f'{seasoningName}', 1)#prints seasoning heading to doc
+        document.add_paragraph(f'{seasoning}')#prints seasoning recipe to doc
+        document.add_heading(f'{condimentName}', 1)#prints condiment heading to doc
+        document.add_paragraph(f'{condiment}')#prints condiment recipe to doc
+        document.add_heading(f'{shellName}', 1)#prints shell heading to doc
+        document.add_paragraph(f'{shell}')#prints shell recipe to doc
+        document.add_page_break()#adds page break after recipe is done
     except ConnectionError:  # output if Connection Error
         print('Connection Error')  # what user sees if Connection Error
     except TimeoutError:  # output if Timeout error
@@ -68,9 +68,7 @@ user for amount of recipes they would like generated.
 '''
 while True:
     try:  # 'trys' to run following code
-        recipes = int(
-            input(
-                'How many recipes would you like in your Random cookbook?\nPlease enter a whole number in numeric form: '))# this line asks user for a whole number of recipes they would like
+        recipes = int(input('How many recipes would you like in your Random cookbook?\nPlease enter a whole number in numeric form: '))# this line asks user for a whole number of recipes they would like
         break  # breaks loop
     except ValueError:  # error exception if wrong input data type
         print('Wrong Data Type. Try again')  # error output to user
@@ -80,14 +78,14 @@ while True:
 This block is meant to generate the tacobook using main looped as many times as the user wanted. 
 '''
 print('Generating TacoBook')#lets user know the tacobook is being generated
-document.add_heading('Random Taco Cookbook', 0)
-document.add_picture('tacobookCover.jpeg')
-document.add_heading('Credits', 1)
-#image
-#api
-#author
-document.add_page_break()
-for number in range(recipes):#for loop to create recipes
+document.add_heading('Random Taco Cookbook', 0)#prints man heading to doc
+document.add_picture('tacobookCover.jpeg')#adds cover photo to doc
+document.add_heading('Credits', 1)#prints credits heading to doc
+document.add_paragraph('Taco image : Photo by Xavier Crook on Unsplash', style='List Bullet')#prints bullet list to doc - photo credit
+document.add_paragraph('Recipes from : https://taco-1150.herokuapp.com/random/?full_taco=true', style='List Bullet')#prints bullet list to doc - API credit
+document.add_paragraph('Code by Reginald Zaccardi-Richey', style='List Bullet')#prints bullet list to doc - My credit
+document.add_page_break()#adds page break after cover
+for number in range(recipes):#For loop to create recipes
     main(url)#runs our main function
 document.save('TacoBook.docx')#saves the document under file name "TacoBook.docx
 print('Your Random Taco Cookbook has been created with the file name \'TacoBook.docx\'')#notifys user the tacobook is complete and the file name
